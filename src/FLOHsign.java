a60c17210eb4aed3cda32abb524cbb50acba2075
Drew Rautenberg <drew@drewrautenberg.com>
2017-09-27 15:17:12 -0400

    implemented O print out

--- src/FLOHsign.java
+++ src/FLOHsign.java
@@ -10,21 +10,40 @@ public class FLOHsign {
         int width;
         char on = 'X';
         char off = '.';
-
+        int i;
+        int j;
 
         //user input
         height = 10;
-        width = 5;
+        width = 10;
 
         //F print out
+
 
 
 
         //L print out
 
         //O print out
-
-
+        //Top horizontal of O
+        for (i = 0; i < width; i++) {
+            System.out.print(on);
+        }
+        System.out.println();
+        //Sides of O
+        for (i = 0; i < height-2; i++) {
+            System.out.print(on);
+            for (j=0;j<width-2;j++){
+                System.out.print(off);
+            }
+            System.out.print(on);
+            System.out.println();
+        }
+        //Bottom horizontal of O
+        for (i = 0; i < width; i++) {
+            System.out.print(on);
+        }
+        System.out.println();
         //H print out
 
     }
