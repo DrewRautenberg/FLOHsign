a42e7ffd367f9f02d793ece4acde33ca2cd38a06
Drew Rautenberg <drew@drewrautenberg.com>
2017-09-27 15:26:17 -0400

    implemented F print out

--- src/FLOHsign.java
+++ src/FLOHsign.java
@@ -12,12 +12,53 @@ public class FLOHsign {
         char off = '.';
         int i;
         int j;
+        int fTopVert;
+        int fBottVert;
 
         //user input
         height = 10;
         width = 10;
 
         //F print out
+        //calculate heights
+        fTopVert = (height-2)/2;
+        if (height % 2 == 0){
+            fBottVert = fTopVert;
+        }
+        else {
+            fBottVert = fTopVert + 1;
+        }
+        //Top of F
+        for (i = 0; i < width; i++) {
+            System.out.print(on);
+        }
+        System.out.println();
+        //Top vertical of F
+        for (i=0;i<fTopVert;i++){
+            System.out.print(on);
+            for (j=0;j<width-1;j++){
+                System.out.print(off);
+            }
+            System.out.println();
+        }
+        //Middle horizontal of F
+        for (i=0;i<width/2;i++){
+            System.out.print(on);
+        }
+        for (j=i;j<width;j++){
+            System.out.print(off);
+        }
+        System.out.println();
+        //Bottom Vertical of F
+        for (i=0;i<fBottVert;i++){
+            System.out.print(on);
+            for (j=0;j<width-1;j++){
+                System.out.print(off);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
 
 
 
