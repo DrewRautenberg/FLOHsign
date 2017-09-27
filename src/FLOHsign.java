adee60cf95be42f6638cea8f9d05cb154b900b5d
Drew Rautenberg <drew@drewrautenberg.com>
2017-09-27 15:06:26 -0400

    variable declaration

--- src/FLOHsign.java
+++ src/FLOHsign.java
@@ -1,10 +1,15 @@
 /* PROJECT:  FLOHsign
  * AUTHOR: Drew Rautenberg
  * DESCRIPTION:
- * 
+ *
  *****************************************************************************/
 public class FLOHsign {
     public static void main(String[] args) {
+        //variable declaration
+        int height;
+        int width;
+        char on = 'X';
+        char off = '.';
 
     }
 }
